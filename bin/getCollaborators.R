bf54035b1581a1d46eda8a8cb3677e05079e065c
justindemonte <42752488+justindemonte@users.noreply.github.com>
2018-11-12 11:29:01 -0500

    Update getCollaborators.R

--- bin/getCollaborators.R
+++ bin/getCollaborators.R
@@ -4,24 +4,33 @@ library(readr)
 
 args = commandArgs(trailingOnly = TRUE)
 
+infile <- read_lines(args[1])
 
-abstract <- read_file(args[1])
-abstract <- str_replace_all(abstract, "[\r\n]" , "")
+index <- match(1, str_detect(pattern = "Author information", infile))
 
+# these variables hold the author info from the file and the "abstract proper" from the file, respectively
+
+authorInfo <- infile[index]
+
+abstract <- infile[index+1]
+
+# Gets the Collaborators 
 
 keyWordRegex <- '(University|Hospital|School|Institute|Center)'
 
-SplitAbstractCommas <- as.list(str_split(abstract, pattern = ",")[[1]])
+SplitAuthorInfo <- as.list(str_split(authorInfo, pattern = ",")[[1]])
 
-CollaboratorsFromAbstract <- SplitAbstractCommas[grepl(keyWordRegex, SplitAbstractCommas)]
+CollaboratorsFromAbstract <- SplitAuthorInfo[grepl(keyWordRegex, SplitAuthorInfo)]
 
 Collaborators <- unlist(CollaboratorsFromAbstract)
 
 Collaborators <- unique(Collaborators)
 
-abstractVector <- list2 <- rep(abstract,length(Collaborators))
+# Creates tibble and output csv
 
-output <- tibble(abstract = abstractVector, Collab = Collaborators)
+abstractVector <- rep(abstract,length(Collaborators))
+
+output <- tibble(abstrct = abstractVector, Collab = Collaborators)
 
 write_csv(output, "out.csv")
 

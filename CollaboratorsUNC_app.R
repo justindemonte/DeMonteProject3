a4d943d0ee4b3da8dfb6bf0ff56ea85d5f5ad861
justindemonte <42752488+justindemonte@users.noreply.github.com>
2018-11-14 00:50:25 -0500

    Update CollaboratorsUNC_app.R

--- CollaboratorsUNC_app.R
+++ CollaboratorsUNC_app.R
@@ -12,16 +12,20 @@ TopTenCollaborators <- read_csv("wordsKey.csv")
 
 
 # Use a fluid Bootstrap layout
+
 ui <- fluidPage(    
   
   # Give the page a title
+  
   titlePanel("Top UNC Collaborators and Subjects"),
   
   # Generate a row with a sidebar
+  
   sidebarLayout(      
     
     
     # Define the sidebar with one input
+    
     sidebarPanel(
       selectInput("region", "Collaborator:", 
                   choices=TopTenCollaborators),
@@ -29,8 +33,8 @@ ui <- fluidPage(
       helpText("Top Ten Institutions Collaborating with UNC"),
       p("Which institutions does The University of North Carolina at Chapel Hill collaborate with most frequently?  What are the likely subject emphases of these collaborations?  To answer this question, I started with the pubmed abstracts provided by Dr. Biggs.  I searched each abstract for institutions names (e.g. phrases containing the keywords university, hospital, etc.) then collected the top ten collaborators.  For each collaborator, I found the most frequently used words in abstracts involving that collaborator.  The results can be explored in this interactive application.  Select one of the top-ten-UNC collaborating institutions from the drop-down menu to explore the likely subject matter of those collaborations.")),
     
-    
     # Create a spot for the barplot
+    
     mainPanel(
       
       plotOutput("wordPlot")  
@@ -43,9 +47,11 @@ ui <- fluidPage(
 library(datasets)
 
 # Define a server for the Shiny app
+
 server <- function(input, output) {
   
   # Fill in the spot created for a plot
+  
   output$wordPlot <- renderPlot({
       read_csv(paste("words", 
           match(input$region,TopTenCollaborators[[1]]), ".csv", sep="")) %>%
@@ -60,5 +66,6 @@ server <- function(input, output) {
 
 
 # Run the application 
+
 shinyApp(ui = ui, server = server)
 

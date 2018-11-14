1a15560ad395d9cf24d82c88a039411fff20cdc6
justindemonte <42752488+justindemonte@users.noreply.github.com>
2018-11-14 00:35:25 -0500

    Update GetTopTen.R

--- bin/GetTopTen.R
+++ bin/GetTopTen.R
@@ -26,10 +26,10 @@ TopTenNames <- TopTenNames[c(-2,-5,-6,-7,-9,-14,-17)]
 
 # Create a subset of the original tibble for each of the collaborators
 
-test1 <- 
+Collab <- 
   1:10 %>%
   lapply(function(x) {
-    y = CollaboratorsTbl %>%
+      CollaboratorsTbl %>%
       filter(Collab==TopTenNames[[x]]) %>%
       {paste(unique(.$abstrct), sep="", collapse = " ")} %>%
        str_split(pattern = " ") %>%
@@ -38,22 +38,22 @@ test1 <-
       
   })
 
-test1 <- lapply(test1, . %>% arrange(desc(count)))
+Collab <- lapply(Collab, . %>% arrange(desc(count)))
 
 # remove irrelevant words
 
 FilterStopWords <- c("using","H", "CI,", "In", "This","3","The","We","=", "95%","a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your")
 
-test1 <- lapply(test1, . %>% filter(!words %in% FilterStopWords))
+Collab <- lapply(Collab, . %>% filter(!words %in% FilterStopWords))
 
-test1 <- lapply(test1, . %>% top_n(10))
+Collab <- lapply(Collab, . %>% top_n(10))
 
 # Create output CSV's
 
 write_csv(as.data.frame(TopTenNames), "wordsKey.csv")
 
 for (i in 1:10) {
-  write_csv(test1[[i]], paste("words", i, ".csv", sep=""))
+  write_csv(Collab[[i]], paste("words", i, ".csv", sep=""))
 }
 
 
